20d3aa55f37f786288245df87b02e3e205dd05ae
Anna Filin <68apple@gmail.com>
2025-02-02 11:41:50 +0200

    Initial Commit

---
+++ Weathery/WeatheryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatheryApp.swift
+//  Weathery
+//
+//  Created by Anna Filin on 02/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatheryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
